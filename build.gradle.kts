b3921bcd3a1534ce78106b679ee1c383be1832ad
iamdudeman <timothy.solum@gmail.com>
2022-01-11 20:48:49 -0600

    Initial file structure

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+  id("java-library")
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_16
+  targetCompatibility = JavaVersion.VERSION_16
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  testImplementation(platform("org.junit:junit-bom:5.7.1"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+  useJUnitPlatform()
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+}
