fdbda84c94b7e34aaef23e298abc3094848ec64d
iamdudeman <timothy.solum@gmail.com>
2022-09-05 13:06:48 -0500

    Added some missing JavaDocs

--- src/main/java/technology/sola/json/exception/InvalidAbstractSyntaxTreeException.java
+++ src/main/java/technology/sola/json/exception/InvalidAbstractSyntaxTreeException.java
@@ -1,4 +1,7 @@
 package technology.sola.json.exception;
 
+/**
+ * Exception thrown whenever a JSON AST is not in a valid state.
+ */
 public class InvalidAbstractSyntaxTreeException extends RuntimeException {
 }
