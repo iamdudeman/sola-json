1a1dee138a5fb823cd30df05e8e5518d6e431c9c
iamdudeman <timothy.solum@gmail.com>
2022-10-07 12:14:55 -0500

    Added module-info file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * Defines the sola-json API.
+ */
+module sola.json.main {
+  exports technology.sola.json;
+  exports technology.sola.json.builder;
+  exports technology.sola.json.exception;
+  exports technology.sola.json.mapper;
+  exports technology.sola.json.parser;
+  exports technology.sola.json.serializer;
+  exports technology.sola.json.tokenizer;
+}
