2ec63d204b7c7c14e7c123282df3af5249af57d7
iamdudeman <timothy.solum@gmail.com>
2025-03-12 21:55:02 -0500

    benchmarking with real files

--- src/test/java/technology/sola/json/jmh/SolaJsonBenchmark.java
+++ src/test/java/technology/sola/json/jmh/SolaJsonBenchmark.java
@@ -1,11 +1,16 @@
 package technology.sola.json.jmh;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 import technology.sola.json.JsonElement;
 import technology.sola.json.SolaJson;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
@@ -15,22 +20,48 @@ import java.util.concurrent.TimeUnit;
 @Warmup(iterations = 1, time = 5, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
 public class SolaJsonBenchmark {
+  private String commentsJsonString;
+  private String photosJsonString;
+  private String usersJsonString;
+
   @Setup(Level.Trial)
-  public void loadFiles() {
-    // todo load test files in memory
+  public void loadFiles() throws IOException {
+    commentsJsonString = readFileToString("/performance/comments.json");
+    photosJsonString = readFileToString("/performance/photos.json");
+    usersJsonString = readFileToString("/performance/users.json");
   }
 
   @Benchmark
   public void solaJson(Blackhole blackhole) {
     blackhole.consume(
-      new SolaJson().parse("{}")
+      new SolaJson().parse(commentsJsonString)
+    );
+
+    blackhole.consume(
+      new SolaJson().parse(photosJsonString)
+    );
+
+    blackhole.consume(
+      new SolaJson().parse(usersJsonString)
     );
   }
 
   @Benchmark
   public void gson(Blackhole blackhole) {
     blackhole.consume(
-      new Gson().fromJson("{}", JsonElement.class)
+      JsonParser.parseString(commentsJsonString)
     );
+
+    blackhole.consume(
+      JsonParser.parseString(photosJsonString)
+    );
+
+    blackhole.consume(
+      JsonParser.parseString(usersJsonString)
+    );
+  }
+
+  private String readFileToString(String resourcePath) throws IOException {
+    return Files.readString(new File(getClass().getResource(resourcePath).getFile()).toPath());
   }
 }
